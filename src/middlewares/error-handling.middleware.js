c218b4c0a17eedf2291779d8c0f15b08d923ace5
Yun <ydh1503@naver.com>
2024-05-29 08:57:16 +0900

    Fix - 불필요한 코드 제거

--- src/middlewares/error-handling.middleware.js
+++ src/middlewares/error-handling.middleware.js
@@ -5,9 +5,9 @@ export default function (err, req, res, next) {
   switch (err.name) {
     case 'ValidationError':
       return res.status(401).json({
-        errorMessage: `'${err.details[0].context.key}' 유효성 검사에 실패했습니다. (${err.details[0].context.key} : ${err.details[0].context.value})`,
+        errorMessage: `'${err.details[0].context.key}' 유효성 검사에 실패했습니다. 
+        (${err.details[0].context.key} : ${err.details[0].context.value})`,
       });
-    //case 'JWTExpiresError':
     default:
       // 클라이언트에게 에러 메시지를 전달합니다.
       return res.status(500).json({ errorMessage: '서버 내부 에러가 발생했습니다.' });
--- src/routes/characters.router.js
+++ src/routes/characters.router.js
@@ -143,30 +143,4 @@ router.get('/users/auth/characters/:characterId', authMiddleware, async (req, re
   }
 });
 
-// /** 캐릭터 로그인 API **/
-// router.post('/sign-in', async (req, res, next) => {
-//   try {
-//     const { id, password } = req.body;
-//     const user = await usersPrisma.users.findFirst({ where: { id } });
-
-//     if (!user) return res.status(401).json({ message: '존재하지 않는 ID 입니다.' });
-//     // 입력받은 사용자의 비밀번호와 데이터베이스에 저장된 비밀번호를 비교합니다.
-//     else if (!(await bcrypt.compare(password, user.password)))
-//       return res.status(401).json({ message: '비밀번호가 일치하지 않습니다.' });
-
-//     // 로그인에 성공하면, 사용자의 userId를 바탕으로 토큰을 생성합니다.
-//     const token = jwt.sign(
-//       {
-//         userId: user.userId,
-//       },
-//       process.env.JWT_SECRET_KEY // 비밀 키, dotenv를 이용해서 외부에서 코드를 보더라도 알 수 없도록 구현해야합니다.
-//     );
-
-//     res.header('authorization', `Bearer ${token}`);
-//     return res.status(200).json({ message: '로그인 성공' });
-//   } catch (err) {
-//     next(err);
-//   }
-// });
-
 export default router;

8dcb13f8d2efd149fb66c8bc5e2bea52e4b7ecd3
Yun <ydh1503@naver.com>
2024-05-29 10:06:17 +0900

    Fix - 에러핸들러 출력문 변경

--- src/middlewares/error-handling.middleware.js
+++ src/middlewares/error-handling.middleware.js
@@ -5,8 +5,7 @@ export default function (err, req, res, next) {
   switch (err.name) {
     case 'ValidationError':
       return res.status(401).json({
-        errorMessage: `'${err.details[0].context.key}' 유효성 검사에 실패했습니다. 
-        (${err.details[0].context.key} : ${err.details[0].context.value})`,
+        errorMessage: `'${err.details[0].context.key}' 유효성 검사에 실패했습니다. (${err.details[0].context.key} : ${err.details[0].context.value})`,
       });
     default:
       // 클라이언트에게 에러 메시지를 전달합니다.
